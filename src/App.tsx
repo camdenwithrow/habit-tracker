8122fe4a8982d01bd9acee3b6f2a82ae85c0b341
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-03 16:23:04 -0500

    simple habit tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+// src/App.tsx
+import React, { useState } from 'react';
+import HabitList from './components/HabitList';
 
-function App() {
+const App: React.FC = () => {
+  const [habits, setHabits] = useState([
+    { name: 'Exercise', completed: false },
+    { name: 'Read', completed: true },
+    { name: 'Meditate', completed: false },
+  ]);
+
+  const handleToggle = (index: number) => {
+    const updatedHabits = [...habits];
+    updatedHabits[index].completed = !updatedHabits[index].completed;
+    setHabits(updatedHabits);
+  };
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="p-4 inline-block">
+      <h1 className="text-3xl font-bold mb-4">Habit Tracker</h1>
+      <HabitList habits={habits} onToggle={handleToggle} />
+    </div>
+  );
+};
 
-export default App
+export default App;
